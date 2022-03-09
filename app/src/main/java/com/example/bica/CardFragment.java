2b5c63c7dd07e39f01e1dc57d2c630ffb9514dce
bhyn <bhyn9785@gmail.com>
2022-03-09 18:09:36 +0900

    [FEAT] Creat Fragment(Connect with MainActivity)

--- app/src/main/java/com/example/bica/CardFragment.java
+++ app/src/main/java/com/example/bica/CardFragment.java
@@ -8,40 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CardFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public CardFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CardFragment newInstance(String param1, String param2) {
+    public static CardFragment newInstance() {
         CardFragment fragment = new CardFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +23,6 @@ public class CardFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
--- app/src/main/java/com/example/bica/MainActivity.java
+++ app/src/main/java/com/example/bica/MainActivity.java
@@ -1,19 +1,49 @@
 package com.example.bica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 mViewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private TabLayout tabLayout;
+    int[] arr = new int[]{R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // BICA 개발 공간 입니다.
-        // TODO : 메인 액티비티 개발해야된다.
+        Fragment AddCard = new AddCardFragment().newInstance();
+        Fragment Card = new CardFragment().newInstance();
+        Fragment Favorite = new FavoriteFragment().newInstance();
+        Fragment MyCard = new MyCardFragment().newInstance();
+        Fragment Setting = new SettingFragment().newInstance();
+        mViewPager = findViewById(R.id.viewpager);
+        mViewPager.setOffscreenPageLimit(3);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPagerAdapter.addFrag(Card);
+        viewPagerAdapter.addFrag(Favorite);
+        viewPagerAdapter.addFrag(AddCard);
+        viewPagerAdapter.addFrag(MyCard);
+        viewPagerAdapter.addFrag(Setting);
+        mViewPager.setAdapter(viewPagerAdapter);
 
-        // Test 코드입니다.
+        new TabLayoutMediator(tabLayout, mViewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setIcon(arr[position]);
+            }
+        }).attach();
+
     }
 }
