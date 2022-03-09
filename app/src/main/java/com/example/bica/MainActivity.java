4939a3fda2886667b1a66e246611c054c9b3812a
hwangjunha <junha5021301@kakao.com>
2022-03-09 15:21:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bica/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // BICA 개발 공간 입니다.
+    }
+}
