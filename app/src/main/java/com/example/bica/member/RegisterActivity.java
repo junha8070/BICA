7d37f31f94a4bcd9ae9eec343e7812a9ca86b225
hwangjunha <junha5021301@kakao.com>
2022-03-09 18:01:20 +0900

    [FEAT] Create Register Activity

---
+++ app/src/main/java/com/example/bica/member/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.bica.member;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bica.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // TODO: 회원가입 구현
+    }
+}
