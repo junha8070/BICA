045d9b26dbdd8afd5aad27e1a376a27813fe8c1d
Junha Hwang <junha5021301@kakao.com>
2022-03-11 03:46:17 +0900

    [FEAT] ADD fade_in animation

--- app/src/main/java/com/example/bica/InitialActivity.java
+++ app/src/main/java/com/example/bica/InitialActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.bica.member.LoginActivity;
 import com.example.bica.member.RegisterActivity;
@@ -13,12 +16,23 @@ import com.example.bica.member.RegisterActivity;
 public class InitialActivity extends AppCompatActivity {
 
     private Button btn_login, btn_register;
+    private TextView tv_first, tv_second, tv_third;
+    private Animation fade_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        // 초기화
         init();
+
+        tv_first.setVisibility(View.VISIBLE);
+        tv_first.startAnimation(fade_in);
+        tv_second.setVisibility(View.VISIBLE);
+        tv_second.startAnimation(fade_in);
+        tv_third.setVisibility(View.VISIBLE);
+        tv_third.startAnimation(fade_in);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +53,13 @@ public class InitialActivity extends AppCompatActivity {
     }
 
     public void init(){
+        // 애니메이션 초기화
+        fade_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+
+        // 요소 초기화
+        tv_first = findViewById(R.id.tv_welcome_first);
+        tv_second = findViewById(R.id.tv_welcome_second);
+        tv_third = findViewById(R.id.tv_welcome_third);
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
     }
