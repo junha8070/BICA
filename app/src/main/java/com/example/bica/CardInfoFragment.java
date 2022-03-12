c6ae48c442bae72de55454bf4067e6bcbcbddcbf
daeun <ekqhddl23@naver.com>
2022-03-12 19:37:30 +0900

    mycard,cardinfo ui

--- app/src/main/java/com/example/bica/CardInfoFragment.java
+++ app/src/main/java/com/example/bica/CardInfoFragment.java
@@ -2,7 +2,6 @@ package com.example.bica;
 
 import android.os.Bundle;
 
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,13 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-public class MyCardFragment extends Fragment {
+public class CardInfoFragment extends Fragment {
 
-    public MyCardFragment() {
+    public CardInfoFragment() {
         // Required empty public constructor
     }
-    public static MyCardFragment newInstance() {
-        MyCardFragment fragment = new MyCardFragment();
+    public static CardInfoFragment newInstance() {
+        CardInfoFragment fragment = new CardInfoFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -35,7 +34,7 @@ public class MyCardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_card, container, false);
+        View view = inflater.inflate(R.layout.fragment_card_info, container, false);
         return view;
 
     }
@@ -44,17 +43,17 @@ public class MyCardFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_mycard, menu);
+        inflater.inflate(R.menu.menu_card, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.mycard_share:
+            case R.id.card_share:
                 Toast.makeText(getActivity(), "공유", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.mycard_edit:
-                Toast.makeText(getActivity(), "편집", Toast.LENGTH_SHORT).show();
+            case R.id.card_del:
+                Toast.makeText(getActivity(), "삭제", Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
