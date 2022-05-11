850814a819a9388bc32baf46d850ab0a8aa4888c
aksen510 <ghkdwlsdud510@gmail.com>
2022-05-11 21:33:21 +0900

    Test Github

--- app/src/main/java/com/example/bica/CardInfoFragment.java
+++ app/src/main/java/com/example/bica/CardInfoFragment.java
@@ -23,7 +23,7 @@ public class CardInfoFragment extends Fragment {
     public CardInfoFragment() {
         // Required empty public constructor
     }
-    //TODO: Git test 223
+    //TODO: Git test 223456
     public static CardInfoFragment newInstance() {
         CardInfoFragment fragment = new CardInfoFragment();
         Bundle args = new Bundle();
