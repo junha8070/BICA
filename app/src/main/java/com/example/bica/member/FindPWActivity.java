7288b67ed67fee5961c248f7ae4fc8a8f8fe9cdd
aksen510 <510cara@naver.com>
2022-05-12 23:10:26 +0900

    [FEAT] Add find password function

--- app/src/main/java/com/example/bica/member/FindPWActivity.java
+++ app/src/main/java/com/example/bica/member/FindPWActivity.java
@@ -1,21 +1,39 @@
 package com.example.bica.member;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.bica.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class FindPWActivity extends AppCompatActivity {
 
     private EditText edt_username, edt_phonenum, edt_email;
-    private Button btn_phonenum, btn_email;
+    private Button btn_email;
     private Toolbar tb_findPW;
+    private static final String TAG = "FindPWActivity";
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private boolean checkInfo = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +41,63 @@ public class FindPWActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_pw);
 
         init();
+
+        edt_phonenum.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        btn_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String useremail = edt_email.getText().toString().trim();
+                String username = edt_username.getText().toString().trim();
+                String phonenum = edt_phonenum.getText().toString().trim();
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(username.isEmpty() || phonenum.isEmpty() || useremail.isEmpty()){
+                    Toast.makeText(FindPWActivity.this, "빈칸을 입력해주세요", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    firestore.collection("users")
+                            .whereEqualTo("username", username)
+                            .whereEqualTo("phonenum", phonenum)
+                            .whereEqualTo("email", useremail)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if(task.isSuccessful()){
+                                        Log.d(TAG, "회원정보 일치");
+                                        firebaseAuth.sendPasswordResetEmail(useremail)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if(task.isSuccessful()){
+                                                            Log.d(TAG, "이메일 전송 성공");
+                                                            Toast.makeText(FindPWActivity.this, "이메일을 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                                            Intent starLogin = new Intent(FindPWActivity.this, LoginActivity.class);
+                                                            startActivity(starLogin);
+                                                            finish();
+                                                        }
+                                                    }
+                                                }).addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.e(TAG,"이메일 전송 실패", e);
+                                                        Toast.makeText(FindPWActivity.this, "이메일 전송에 실패하였습니다. 다시 시도해주세요.",Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.e(TAG, "일치하는 회원정보 없음");
+                                    Toast.makeText(FindPWActivity.this, "일치하는 회원정보가 없습니다. 다시 입력해주세요", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+        });
 
         // Toolbar 활성화
         setSupportActionBar(tb_findPW);
@@ -47,7 +122,6 @@ public class FindPWActivity extends AppCompatActivity {
         edt_username = findViewById(R.id.user_name);
         edt_phonenum = findViewById(R.id.user_phonenum);
         edt_email = findViewById(R.id.user_email);
-        btn_phonenum = findViewById(R.id.phonenum_check);
         btn_email = findViewById(R.id.email_check);
     }
 }
